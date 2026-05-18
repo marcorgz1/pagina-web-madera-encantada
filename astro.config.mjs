2a7aa024d54755c5e7db306d2555cfe14b1e64a7
marcorgz1 <marco.valdemoro@gmail.com>
2026-05-18 20:11:29 +0200

    feat: import vercel deployment necessary plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
     vite: {
@@ -11,7 +13,5 @@ export default defineConfig({
         ]
     },
     output: 'server',
-    adapter: node({
-        mode: 'standalone'
-    })
+    adapter: vercel()
 });
