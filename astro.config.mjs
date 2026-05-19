d3c565c8c02cdea63514dec3e90a2603e1c34f2f
marcorgz1 <marco.valdemoro@gmail.com>
2026-05-19 17:17:19 +0200

    minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
 export default defineConfig({
     vite: {
